2f88aa02ee307c56cd3eb8c59714b52ec10e852a
maryamrmz <maryam.rpour@gmail.com>
2025-10-05 23:56:19 +0330

    feat: add tag serializers functionality

--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -26,3 +29,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """View for manage tag APIs"""
+    serializer_class = serializers.TagSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Return tags for authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
