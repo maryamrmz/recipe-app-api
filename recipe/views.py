b0dc057b2cdce85da577025d8b6e41c558eea160
maryamrmz <maryam.rpour@gmail.com>
2025-10-06 20:18:36 +0330

    feat: complete upload image API

--- recipe/views.py
+++ recipe/views.py
@@ -1,27 +1,20 @@
-from rest_framework import (
-    viewsets,
-    mixins,
-    status,
-)
-from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets, mixins, status, parsers
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import (
-    Recipe,
-    Tag,
-    Ingredient
-)
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    """View for manage recipe APIs"""
+    """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Return recipes for authenticated user only"""
@@ -36,29 +29,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return self.serializer_class
 
     def perform_create(self, serializer):
-        """Create a new recipe"""
         serializer.save(user=self.request.user)
 
-    @action(methods=['POST'], detail=True, url_path='upload-image')
+    @extend_schema(
+        request={
+            'multipart/form-data': {
+                'type': 'object',
+                'properties': {
+                    'image': {'type': 'string', 'format': 'binary'}
+                },
+                'required': ['image'],
+            }
+        },
+        responses={200: serializers.RecipeImageSerializer},
+    )
+    @action(
+        methods=['POST'],
+        detail=True,
+        url_path='upload-image',
+        parser_classes=[parsers.MultiPartParser, parsers.FormParser]
+    )
     def upload_image(self, request, pk=None):
-        """Upload an image to a recipe"""
         recipe = self.get_object()
-        serializer = self.get_serializer(
-            recipe,
-            data=request.data,
-        )
-
+        serializer = self.get_serializer(recipe, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK,
-            )
-
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
@@ -78,13 +75,9 @@ class TagViewSet(BaseRecipeAttrViewSet):
     """View for manage tag APIs"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
 
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
     """View for manage ingredient APIs"""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
